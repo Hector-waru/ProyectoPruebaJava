f1f95b2598e976a881fefb00997ab0d99bc0ba0a
Hector Garcia Ortiz <Hector.Garcia.Ortiz@gmail.com>
2022-01-27 15:59:28 -0600

    Proyecto prueba

---
+++ Proyecto-Prueba/src/PrimeraClase.java
@@ -0,0 +1,8 @@
+//abrimos nuestro archivo como openjava visor
+//mayormente utilizaremos clases
+//Upper camel case (mayuscula en cada palabra)
+public class PrimeraClase {
+	public static void main(String[] args) {
+		System.out.println("Hola mundo desde java");
+		}
+}
